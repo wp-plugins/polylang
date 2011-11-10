2b6a74c7a166ae750770ef68220eec612bb4421d
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-10 16:22:36 +0000

    release v0.4
    git-svn-id: https://plugins.svn.wordpress.org/polylang/tags/0.4@461421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/edit-term-form.php
+++ include/edit-term-form.php
@@ -7,7 +7,12 @@
 	<td><select name="term_lang_choice" id="term_lang_choice">
 		<option value="0"></option><?php
 		foreach ($listlanguages as $language) {
-			printf("<option value='%s'%s>%s</option>\n", $language->term_id, $language == $lang ? ' selected="selected"' : '', $language->name);
+			printf(
+				"<option value='%s'%s>%s</option>\n",
+				esc_attr($language->term_id),
+				$language == $lang ? ' selected="selected"' : '',
+				esc_attr($language->name)
+			);
 		} ?>
 	</select><br />
 	<span class="description"><?php _e('Sets the language', 'polylang');?></span></td>
@@ -16,5 +21,5 @@
 <tr id="term-translations" class="form-field"><?php
 // do not display translation fields if term language is not set
 if ($lang)
-	include(POLYLANG_DIR.'/term-translations.php');?>
+	include(INC_DIR.'/term-translations.php');?>
 </tr>
