66d8d7e35cc85ac6ef75bc510805d49597b46ae5
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-07 20:13:22 +0000

    release v0.5
    git-svn-id: https://plugins.svn.wordpress.org/polylang/tags/0.5@472169 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/post-metabox.php
+++ include/post-metabox.php
@@ -1,22 +1,22 @@
-<?php // allowing to choose the post's language 
-// NOTE: the class "tags-input" allows to include the field in the autosave $_POST (see autosave.js)?>
+<?php
+// allowing to choose the post's language 
+// NOTE: the class "tags-input" allows to include the field in the autosave $_POST (see autosave.js)
+?>
 <p><em><?php $post_type == 'page' ? _e('Page\'s language:', 'polylang') : _e('Post\'s language:', 'polylang');?></em></p>
 <p>
-<select name="post_lang_choice" id="post_lang_choice">
-	<option value="0"></option> <?php
+<select name="post_lang_choice" id="post_lang_choice"><?php
+	if (PLL_DISPLAY_ALL) // for those who want undefined language
+		echo '<option value="0"></option>';
 	foreach ($listlanguages as $language) {
 		printf(
 			"<option value='%s'%s>%s</option>\n",
 			esc_attr($language->slug),
 			$language == $lang ? ' selected="selected"' : '',
-			esc_attr($language->name)
+			esc_html($language->name)
 		);
 	} ?>
 	</select><br />
 </p>
-<div id="post-translations">
-<?php // allowing to determine the linked posts
-if (isset($lang))
-	include(INC_DIR.'/post-translations.php');
-?>
+<div id="post-translations"><?php
+include(PLL_INC.'/post-translations.php'); // allowing to determine the linked posts ?>
 </div>
--- include/term-translations.php
+++ include/term-translations.php
@@ -1,14 +1,15 @@
 <?php
 // displays the translations fields
 
-if (isset($term_id)) { // edit term form?>
+if (isset($term_id)) {
+	// edit term form?>
 	<th scope="row" valign="top"><?php _e('Translations', 'polylang');?></th>
 	<td><?php
 }
-else { // add term form?>
+else {
+	// add term form?>
 	<label><?php _e('Translations', 'polylang');?></label><?php
-}
-?>
+}?>
 <table>
 	<thead><tr><?php
 		foreach (array(__('Language', 'polylang'), __('Translation', 'polylang'), __('Edit', 'polylang')) as $title)
@@ -20,26 +21,26 @@ else { // add term form?>
 
 			// look for any existing translation in this language
 			if ($language != $lang) {
-				if (isset($term_id) && $translation_id = $this->get_translated_term($term_id, $language))
+				if (isset($term_id) && $translation_id = $this->get_translation('term', $term_id, $language))
 					$translation = get_term($translation_id, $taxonomy);
 				if (isset($_GET['from_tag']) && isset($_GET['from_lang'])) {
 					if ($_GET['from_lang'] == $language->slug)
 						$translation = get_term($_GET['from_tag'], $taxonomy);
-					elseif ($translation_id = $this->get_translated_term($_GET['from_tag'], $language))
+					elseif ($translation_id = $this->get_translation('term', $_GET['from_tag'], $language))
 						$translation = get_term($translation_id, $taxonomy);
 				}?>
 
-				<tr><td><?php echo esc_attr($language->name);?></td><?php
+				<tr><td><?php echo esc_html($language->name);?></td><?php
 
 				// no translation exits in this language
 				if (!$translation) {
 					$translations = $this->get_terms_not_translated($taxonomy, $language, $lang);
 					if (!empty($translations)) { ?>
 						<td>
-							<select name="_lang-<?php echo esc_attr($language->slug);?>" id="_lang-<?php echo esc_attr($language->slug);?>">
+							<select name="tr_lang[<?php echo esc_attr($language->slug);?>]" id="tr_lang_<?php echo esc_attr($language->slug);?>">
 								<option value="0"></option><?php
 								foreach ($translations as $translation) { ?>
-									<option value="<?php echo esc_attr($translation->term_id);?>"><?php echo esc_attr($translation->name);?></option><?php
+									<option value="<?php echo esc_attr($translation->term_id);?>"><?php echo esc_html($translation->name);?></option><?php
 								} ?>
 							</select>
 						</td><?php
@@ -52,7 +53,7 @@ else { // add term form?>
 						// do not display the add new link in add term form ($term_id not set !!!)
 						if (isset($term_id)) {
 							$link = esc_url(admin_url(sprintf(
-								'edit-tags.php?taxonomy=%s&amp;from_tag=%s&amp;from_lang=%s&amp;new_lang=%s',
+								'edit-tags.php?taxonomy=%1$s&from_tag=%2$d&from_lang=%3$s&new_lang=%4$s',
 								$taxonomy,
 								$term_id,
 								$lang->slug,
@@ -64,11 +65,12 @@ else { // add term form?>
 				}
 
 				// a translation exists
-				else { ?>
-					<td><?php echo esc_attr($translation->name); ?></td>									
+				else {
+					printf('<td><input type="hidden" name="tr_lang[%s]" value="%d" />%s</td>',
+						esc_attr($language->slug), esc_attr($translation->term_id) ,esc_html($translation->name));?>					
 					<td><?php
 						$link = esc_url(admin_url(sprintf(
-							'edit-tags.php?action=edit&amp;taxonomy=%s&amp;tag_ID=%s',
+							'edit-tags.php?action=edit&amp;taxonomy=%1$s&tag_ID=%2$d',
 							$taxonomy,
 							$translation->term_id
 						)));
@@ -79,7 +81,9 @@ else { // add term form?>
 			} // if (!$value)
 		} // foreach ?>
 	</tbody>
-</table>
-<?php if (isset($term_id)) { // edit term form?>
-</td><?php
+</table><?php
+
+if (isset($term_id)) {
+	// edit term form?>
+	</td><?php
 }
