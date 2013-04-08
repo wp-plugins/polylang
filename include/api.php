a236011cd552ea44f95dc634b27db8a356f7602b
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 18:55:03 +0000

    release 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/polylang/tags/1.0.4@693864 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/api.php
+++ include/api.php
@@ -1,51 +1,97 @@
 <?php
 
-// template tag: displays the language switcher
+/*
+ * template tag: displays the language switcher
+ */
 function pll_the_languages($args = '') {
 	global $polylang;
-	return isset($polylang) ? $polylang->the_languages($args) : '';
+	return class_exists('Polylang_Core') && $polylang instanceof Polylang_Core ? $polylang->the_languages($args) : '';
 }
 
-// returns the current language
+/*
+ * returns the current language
+ */
 function pll_current_language($args = 'slug') {
 	global $polylang;
-	return isset($polylang) ? $polylang->current_language($args) : false;
+	return !(class_exists('Polylang_Core') && $polylang instanceof Polylang_Core && isset($polylang->curlang)) ? false :
+		($args == 'name' ? $polylang->curlang->name :
+		($args == 'locale' ? $polylang->curlang->description :
+		$polylang->curlang->slug));
 }
 
-// among the post and its translations, returns the id of the post which is in the language represented by $slug
+/*
+ * returns the default language
+ */
+function pll_default_language($args = 'slug') {
+	global $polylang;
+	return !(isset($polylang) && ($options = get_option('polylang')) && isset($options['default_lang']) && $lang = $polylang->get_language($options['default_lang'])) ? false :
+		($args == 'name' ? $lang->name :
+		($args == 'locale' ? $lang->description :
+		$lang->slug));
+}
+
+/*
+ * among the post and its translations, returns the id of the post which is in the language represented by $slug
+ */
 function pll_get_post($post_id, $slug = false) {
 	global $polylang;
-	$slug = $slug ? $slug : pll_current_language();
-	return isset($polylang) && $slug ? $polylang->get_post($post_id, $slug) : null;
+	return isset($polylang) && ($slug = $slug ? $slug : pll_current_language()) ? $polylang->get_post($post_id, $slug) : null;
 }
 
-// among the term and its translations, returns the id of the term which is in the language represented by $slug
+/*
+ * among the term and its translations, returns the id of the term which is in the language represented by $slug
+ */
 function pll_get_term($term_id, $slug = false) {
 	global $polylang;
-	$slug = $slug ? $slug : pll_current_language();
-	return isset($polylang) && $slug ? $polylang->get_term($term_id, $slug) : null;
+	return isset($polylang) && ($slug = $slug ? $slug : pll_current_language()) ? $polylang->get_term($term_id, $slug) : null;
 }
 
-// returns the home url in the right language
+/*
+ * returns the home url in the current language
+ */
 function pll_home_url() {
 	global $polylang;
-	return isset($polylang) ? $polylang->get_home_url() : home_url('/');
+	return class_exists('Polylang_Core') && $polylang instanceof Polylang_Core ? $polylang->get_home_url() : home_url('/');
 }
 
-// register strings for translation in the "strings translation" panel
+/*
+ * registers a string for translation in the "strings translation" panel
+ */
 function pll_register_string($name, $string, $multiline = false) {
 	global $polylang;
-	if (isset($polylang) && is_admin())
+	if (class_exists('Polylang_Admin_Base') && $polylang instanceof Polylang_Admin_Base)
 		$polylang->register_string($name, $string, $multiline);
 }
 
-// translates string (previously registered with pll_register_string)
+/*
+ * translates a string (previously registered with pll_register_string)
+ */
 function pll__($string) {
 	return __($string, 'pll_string');
 }
 
-// echoes translated string (previously registered with pll_register_string)
+/*
+ * echoes a translated string (previously registered with pll_register_string)
+ */
 function pll_e($string) {
 	_e($string, 'pll_string');
 }
-?>
+
+/*
+ * returns true if Polylang manages languages and translation for this post type
+ * won't work before the action 'wp_loaded' has been fired
+ */
+function pll_is_translated_post_type($post_type) {
+	global $polylang;
+	return isset($polylang) && is_array($polylang->post_types) && in_array($post_type, $polylang->post_types);
+}
+
+/*
+ * returns true if Polylang manages languages and translation for this taxonomy
+ * won't work before the action 'wp_loaded' has been fired
+ */
+function pll_is_translated_taxonomy($tax) {
+	global $polylang;
+	return isset($polylang) && is_array($polylang->taxonomies) && in_array($tax, $polylang->taxonomies);
+}
+
