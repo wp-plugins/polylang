3d06fbc1869daf526112b944b659afdbaabc7710
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-13 20:36:24 +0000

    release v1.1.6
    git-svn-id: https://plugins.svn.wordpress.org/polylang/tags/1.1.6@787313 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/admin-base.php
+++ include/admin-base.php
@@ -26,19 +26,23 @@ class Polylang_Admin_Base extends Polylang_Base {
 
 	// set user preferences
 	function admin_init_base() {
+		if (!$languages = $this->get_languages_list())
+			return;
+
 		// set text direction if the user set its own language
-		global $wpdb, $wp_locale;
-		$lang_id = $wpdb->get_var($wpdb->prepare("SELECT t.term_id FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id
-			WHERE tt.taxonomy = 'language' AND tt.description = %s LIMIT 1", get_locale())); // no function exists to get term by description
-		if ($lang_id)
-			$wp_locale->text_direction = get_metadata('term', $lang_id, '_rtl', true) ? 'rtl' : 'ltr';
+		$locale = get_locale();
+		foreach($languages as $lang) {
+			if ($locale == $lang->description) {
+				$GLOBALS['wp_locale']->text_direction = get_metadata('term', $lang->term_id, '_rtl', true) ? 'rtl' : 'ltr';
+				$GLOBALS['l10n']['pll_string'] = $this->mo_import($lang);
+				break;
+			}
+		}
 
 		// set user meta when choosing to filter content by language
-		if (isset($_GET['lang']) && $_GET['lang'] && !is_numeric($_GET['lang'])) // numeric when editing a language
+ 		// $_GET[lang] is used in ajax 'tag suggest' and is numeric when editing a language
+		if (!defined('DOING_AJAX') && !empty($_GET['lang']) && !is_numeric($_GET['lang']))
 			update_user_meta(get_current_user_id(), 'pll_filter_content', ($lang = $this->get_language($_GET['lang'])) ? $lang->slug : '');
-
-		if (!$this->get_languages_list())
-			return;
 
 		// adds the languages in admin bar
 		// FIXME: OK for WP 3.2 and newer (the admin bar is not displayed on admin side for WP 3.1)
@@ -47,31 +51,31 @@ class Polylang_Admin_Base extends Polylang_Base {
 
 	// adds the link to the languages panel in the wordpress admin menu
 	function add_menus() {
-		add_submenu_page('options-general.php', __('Languages', 'polylang'), __('Languages', 'polylang'), 'manage_options', 'mlang',  array(&$this, 'languages_page'));
-
-		// adds the about box the languages admin panel
-		// test of $_GET['tab'] avoids displaying the automatically generated screen options on other tabs
-		if (PLL_DISPLAY_ABOUT && isset($_GET['page']) && $_GET['page'] == 'mlang' && (!isset($_GET['tab']) || $_GET['tab'] == 'lang'))
-			add_meta_box('pll_about_box', __('About Polylang', 'polylang'), array(&$this,'about'), 'settings_page_mlang', 'normal', 'high');
+		add_submenu_page('options-general.php', $title = __('Languages', 'polylang'), $title, 'manage_options', 'mlang',  array(&$this, 'languages_page'));
 	}
 
 	// setup js scripts & css styles (only on the relevant pages)
 	function admin_enqueue_scripts() {
 		$screen = get_current_screen();
+		$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
 
+		// for each script:
+		// 0 => the pages on which to load the script
+		// 1 => the scripts it needs to work
+		// 2 => 1 if loaded even if languages have not been defined yet, 0 otherwise
 		$scripts = array(
-			'admin' => array( array('settings_page_mlang'), array('jquery', 'wp-ajax-response', 'postbox') ),
-			'post'  => array( array('post', 'media', 'async-upload', 'edit'),  array('jquery', 'wp-ajax-response') ),
-			'term'  => array( array('edit-tags'), array('jquery', 'wp-ajax-response') ),
-			'user'  => array( array('profile', 'user-edit'), array('jquery') ),
+			'admin' => array( array('settings_page_mlang'), array('jquery', 'wp-ajax-response', 'postbox'), 1 ),
+			'post'  => array( array('post', 'media', 'async-upload', 'edit'),  array('jquery', 'wp-ajax-response'), 0 ),
+			'term'  => array( array('edit-tags'), array('jquery', 'wp-ajax-response'), 0 ),
+			'user'  => array( array('profile', 'user-edit'), array('jquery'), 0 ),
 		);
 
 		foreach ($scripts as $script => $v)
-			if (in_array($screen->base, $v[0]))
-				wp_enqueue_script('pll_'.$script, POLYLANG_URL .'/js/'.$script.'.js', $v[1], POLYLANG_VERSION);
-			
+			if (in_array($screen->base, $v[0]) && ($v[2] || $this->get_languages_list()))
+				wp_enqueue_script('pll_'.$script, POLYLANG_URL .'/js/'.$script.$suffix.'.js', $v[1], POLYLANG_VERSION);
+
 		if (in_array($screen->base, array('settings_page_mlang', 'post', 'edit-tags', 'edit', 'upload', 'media')))
-			wp_enqueue_style('polylang_admin', POLYLANG_URL .'/css/admin.css', array(), POLYLANG_VERSION);
+			wp_enqueue_style('polylang_admin', POLYLANG_URL .'/css/admin'.$suffix.'.css', array(), POLYLANG_VERSION);
 	}
 
 	// downloads mofiles
@@ -122,19 +126,27 @@ class Polylang_Admin_Base extends Polylang_Base {
 			// try to download the file
 			foreach ($versions as $version) {
 				$resp = wp_remote_get($base."$version/messages/$locale.mo", $args + array('filename' => $mofile));
-				if (is_wp_error($resp) || 200 != $resp['response']['code'])
+				if (is_wp_error($resp) || 200 != $resp['response']['code']) {
+					unlink($mofile); // otherwise we download a gzipped 404 page
 					continue;
-
+				}
 				// try to download ms and continents-cities files if exist (will not return false if failed)
 				// with new files introduced in WP 3.4
-				foreach (array("ms", "continent-cities", "admin", "admin-network") as $file)
-					wp_remote_get($base."$version/messages/$file-$locale.mo", $args + array('filename' => WP_LANG_DIR."/$file-$locale.mo"));
-
+				foreach (array('ms', 'continents-cities', 'admin', 'admin-network') as $file) {
+					$resp = wp_remote_get($base."$version/messages/$file-$locale.mo", $args + array('filename' => WP_LANG_DIR."/$file-$locale.mo"));
+					if (is_wp_error($resp) || 200 != $resp['response']['code'])
+						unlink(WP_LANG_DIR."/$file-$locale.mo");
+				}
 				// try to download theme files if exist (will not return false if failed)
 				// FIXME not updated when the theme is updated outside a core update
-				foreach (array("twentyten", "twentyeleven", "twentytwelve") as $theme)
-					wp_remote_get($base."$version/messages/$theme/$locale.mo", $args + array('filename' => get_theme_root()."/$theme/languages/$locale.mo"));
+				foreach (array('twentyten', 'twentyeleven', 'twentytwelve', 'twentythirteen') as $theme) {
+					if (!is_dir($theme_dir = get_theme_root()."/$theme/languages"))
+						continue; // the theme is not present
 
+					$resp = wp_remote_get($base."$version/messages/$theme/$locale.mo", $args + array('filename' => "$theme_dir/$locale.mo"));
+					if (is_wp_error($resp) || 200 != $resp['response']['code'])
+						unlink("$theme_dir/$locale.mo");
+				}
 				return true;
 			}
 		}
@@ -143,23 +155,29 @@ class Polylang_Admin_Base extends Polylang_Base {
 	}
 
 	// returns options available for the language switcher (menu or widget)
-	// FIXME do not include the dropdown in menu yet since I need to work on js
 	function get_switcher_options($type = 'widget', $key ='string') {
-		$options = array (
-			'show_names' => array('string' => __('Displays language names', 'polylang'), 'default' => 1),
-			'show_flags' => array('string' => __('Displays flags', 'polylang'), 'default' => 0),
-			'force_home' => array('string' => __('Forces link to front page', 'polylang'), 'default' => 0),
+		$options = array(
+			'show_names'   => array('string' => __('Displays language names', 'polylang'), 'default' => 1),
+			'show_flags'   => array('string' => __('Displays flags', 'polylang'), 'default' => 0),
+			'force_home'   => array('string' => __('Forces link to front page', 'polylang'), 'default' => 0),
 			'hide_current' => array('string' => __('Hides the current language', 'polylang'), 'default' => 0),
 		);
-		$menu_options = array('switcher' => array('string' => __('Displays a language switcher at the end of the menu', 'polylang'), 'default' => 0));
-		$widget_options = array('dropdown' => array('string' => __('Displays as dropdown', 'polylang'), 'default' => 0));
-		$options = ($type == 'menu') ? array_merge($menu_options, $options) : array_merge($options, $widget_options);
+
+		if ($type != 'menu')
+			$options['dropdown'] = array('string' => __('Displays as dropdown', 'polylang'), 'default' => 0);
+
 		return array_map(create_function('$v', "return \$v['$key'];"), $options);
 	}
 
 	// register strings for translation making sure it is not duplicate or empty
-	function register_string($name, $string, $multiline = false) {
-		$to_register = array('name'=> $name, 'string' => $string, 'multiline' => $multiline);
+	function register_string($name, $string, $context = 'polylang', $multiline = false) {
+		// backward compatibility with Polylang older than 1.1
+		if (is_bool($context)) {
+			$multiline = $context;
+			$context = 'polylang';
+		}
+
+		$to_register = compact('name', 'string', 'context', 'multiline');
 		if (!in_array($to_register, $this->strings) && $to_register['string'])
 			$this->strings[] = $to_register;
 	}
@@ -169,8 +187,10 @@ class Polylang_Admin_Base extends Polylang_Base {
 		$url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 		// $_GET['lang'] is numeric when editing a language, not when selecting a new language in the filter
-		$selected = isset($_GET['lang']) && $_GET['lang'] && !is_numeric($_GET['lang']) && ($lang = $this->get_language($_GET['lang'])) ? $lang->slug : 
+		$selected = !empty($_GET['lang']) && !is_numeric($_GET['lang']) && ($lang = $this->get_language($_GET['lang'])) ? $lang->slug :
 			(($lg = get_user_meta(get_current_user_id(), 'pll_filter_content', true)) ? $lg : 'all');
+
+		$all_item = array((object) array('slug' => 'all', 'name' => __('Show all languages', 'polylang')));
 
 		$wp_admin_bar->add_menu(array(
 			'id'     => 'languages',
@@ -178,25 +198,21 @@ class Polylang_Admin_Base extends Polylang_Base {
 			'meta'  => array('title' => __('Filters content by language', 'polylang')),
 		));
 
-		$wp_admin_bar->add_menu(array(
-			'parent' => 'languages',
-			'id'     => 'all',
-			'title'  => sprintf('<input name="all" type="radio" value="1" %s /> %s',
-				$selected == 'all' ? 'checked="checked"' : '', __('Show all languages', 'polylang')),
-			'href'   => add_query_arg('lang', 'all', $url),
-		));
-
-		foreach ($this->get_languages_list() as $lang) {
+		foreach (array_merge($all_item, $this->get_languages_list()) as $lang) {
+			$href = esc_url(add_query_arg('lang', $lang->slug, $url));
 			$wp_admin_bar->add_menu(array(
 				'parent' => 'languages',
 				'id'     => $lang->slug,
-				'title'  => sprintf('<input name="%s" type="radio" value="1" %s /> %s',
-					$lang->slug, $selected == $lang->slug ? 'checked="checked"' : '', $lang->name),
-				'href'   => add_query_arg('lang', $lang->slug, $url),
-			));			
+				'title'  => sprintf(
+					'<input name="language" type="radio" onclick="location.href=%s" value="%s" %s /> %s', // FIXME this works but produces invalid html
+					"'" . $href . "'", // onclick is needed for Chrome browser, thanks to RavanH for the bug report and fix
+					esc_attr($lang->slug),
+					$selected == $lang->slug ? 'checked="checked"' : '',
+					esc_html($lang->name)
+				),
+				'href'   => $href,
+			));
 		}
 	}
 
 } // class Polylang_Admin_Base
-
-?>
