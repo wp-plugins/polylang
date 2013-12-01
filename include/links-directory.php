f9e962868f80a45b5d7b57be17f3981fa62372ea
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-01 15:55:45 +0000

    fix archive translation url again
    git-svn-id: https://plugins.svn.wordpress.org/polylang/trunk@813237 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/links-directory.php
+++ include/links-directory.php
@@ -68,7 +68,7 @@ class PLL_Links_Directory {
 		if (!empty($languages)) {
 			global $wp_rewrite;
 			$pattern = '#' . ($this->options['rewrite'] ? '' : '\/language') . '\/('.implode('|', $languages).')\/#';
-			$url = preg_replace($pattern, $wp_rewrite->root, $url);
+			$url = preg_replace($pattern, $wp_rewrite->root . '/', $url);
 		}
 		return $url;
 	}
