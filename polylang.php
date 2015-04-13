162b2ae3e48e59ddd70038ceb511f586cedfda1f
Chouby <Chouby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 18:26:14 +0000

    fix WPML strings translations
    git-svn-id: https://plugins.svn.wordpress.org/polylang/trunk@1133870 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- polylang.php
+++ polylang.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Polylang
 Plugin URI: http://polylang.wordpress.com/
-Version: 1.7.3.1
+Version: 1.7.3.2
 Author: Frédéric Demarle
 Description: Adds multilingual capability to WordPress
 Text Domain: polylang
@@ -33,7 +33,7 @@ Domain Path: /languages
 if (!function_exists('add_action'))
 	exit();
 
-define('POLYLANG_VERSION', '1.7.3.1');
+define('POLYLANG_VERSION', '1.7.3.2');
 define('PLL_MIN_WP_VERSION', '3.8');
 
 define('POLYLANG_BASENAME', plugin_basename(__FILE__)); // plugin name as known by WP
@@ -88,7 +88,7 @@ class Polylang {
 
 		// WPML API
 		if (!defined('PLL_WPML_COMPAT') || PLL_WPML_COMPAT)
-			require_once (PLL_INC.'/wpml-compat.php');
+			PLL_WPML_Compat::instance();
 
 		// extra code for compatibility with some plugins
 		if (!defined('PLL_PLUGINS_COMPAT') || PLL_PLUGINS_COMPAT)
--- readme.txt
+++ readme.txt
@@ -70,6 +70,7 @@ See http://polylang.wordpress.com/documentation/contribute/
 = 1.7.4 =
 
 * fix: translated taxonomies and post types from wpml-config.xml are not filtered on frontend (introduced in 1.7.2)
+* fix: WPML strings translations not always loaded (introduced in 1.7)
 
 = 1.7.3 (2015-04-11) =
 
